9240a22c3136ef729b1795cd9b640658c15e9a57
PatPetChav <paelpec@yahoo.com>
2022-05-02 23:13:10 -0300

    semana03-dia5-final

--- semana03/dia5/main.py
+++ semana03/dia5/main.py
@@ -51,6 +51,75 @@ def setAlumno():
        "message":"registro insertado exitosamente"
     })
 
+@app.route('/alumno/<id>')
+def getAlumnobyID(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute("select * from tbl_alumno where alumno_id = '" + id + "'")
+    
+    data = cursor.fetchall()
+    
+    cursor.close()
+    
+    print(data)
+    
+    return jsonify({
+        'ok':True,
+        "message":"datos de alumnos",
+        "content":data
+    })
+
+@app.route('/alumno/<id>',methods=['PUT'])
+def updateAlumno(id):
+    nombre = request.json['nombre']
+    email = request.json['email']
+    celular = request.json['celular']
+    github = request.json['github']
+
+    cursor = mysql.connection.cursor()
+    sqlUpdateAlumno = "update tbl_alumno set "
+    sqlUpdateAlumno += "alumno_nombre='"+ nombre +"',alumno_email='"+ email +"'"
+    sqlUpdateAlumno += ",alumno_celular='"+ celular+"',alumno_github='"+ github +"' "
+    sqlUpdateAlumno += "where alumno_id = '"+ id +"'"
+    cursor.execute(sqlUpdateAlumno)
+
+    mysql.connection.commit()
+    
+    cursor.close()
+    
+    return jsonify({
+        "ok":True,
+        "message":"registro actualizado exitosamente"
+    })
+
+@app.route('/alumno/<id>',methods=['DELETE'])
+def deleteAlumno(id):
+
+    try :
+        
+        cursor = mysql.connection.cursor()
+        sqlDeleteAlumno = "delete from tbl_alumno "
+        sqlDeleteAlumno += "where alumno_id = '"+ id +"'"
+
+        cursor.execute(sqlDeleteAlumno)
+
+        mysql.connection.commit()
+    
+        cursor.close()
+    
+        return jsonify({
+            "ok":True,
+            "message":"registro borrado exitosamente"
+        })
+    except Exception as err :
+        print(err.__str__())
+        return jsonify({
+            "ok": False,
+            "message": "Error al ejecutar delete : " + err.__str__()
+        })
+
+
+
+
 
 
     
