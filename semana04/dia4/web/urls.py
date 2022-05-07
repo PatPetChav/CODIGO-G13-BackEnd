178d6e52d80eabde74a93cde4b206333ec920c47
PatPetChav <paelpec@yahoo.com>
2022-05-07 01:00:07 -0300

    semana4-dia5-carrito

--- semana04/dia4/web/urls.py
+++ semana04/dia4/web/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'web'
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('producto/<int:producto_id>',views.producto,name='producto'),
+    path('productosPorCategoria/<int:categoria_id>',views.productosPorCategoria,name='productosPorCategoria'),
+    path('carrito',views.carrito,name='carrito'),
+    path('agregarCarrito/<int:producto_id>',views.agregarCarrito,name='agregarCarrito')
 ]
