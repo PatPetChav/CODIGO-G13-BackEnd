d8f8a32eecd49a52485c36db7c1dc58df5f4a58c
unknown <paelpec@yahoo.com>
2022-05-13 17:46:33 -0500

    inicio

--- semana01/dia2/buclefor.py
+++
@@ -1,3 +0,0 @@
-nombre = input("ingrese tu nombre : ")
-for n in nombre:
-    print(n)
--- semana01/dia2/functionprint.py
+++
@@ -1,9 +0,0 @@
-print("hola")
-print("mundo")
-
-print("hola", end=" ")
-print("mundo")
-
-print("*", end = "-")
-print("-"*5, end = "")
-print("*")
