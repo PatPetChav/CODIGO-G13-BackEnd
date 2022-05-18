50406111f9ed32766babf9e1f2092bf2c9cfa954
unknown <paelpec@yahoo.com>
2022-05-17 19:53:13 -0500

    semana05-dia5-final

--- semana05/dia05/pos_frontend/src/environments/environments.js
+++ semana05/dia05/pos_frontend/src/environments/environments.js
@@ -1,3 +1,4 @@
-export const URL_BACKEND = 'http://127.0.0.1:8000'
+export const URL_BACKEND = 'https://pperezposbackend.herokuapp.com'
+//'http://127.0.0.1:8000'
 // 'https://backposapi.herokuapp.com'
 	
