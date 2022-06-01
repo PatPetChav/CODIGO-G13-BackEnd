fea17d48ff9d733486a27fb2ce832ba90bcd9d2c
unknown <paelpec@yahoo.com>
2022-05-31 23:02:27 -0500

    express-get-post-put-delete

---
+++ semana08/dia2/database.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+
+const mysqlConnection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'db_colegio'
+});
+
+// mysqlConnection.connect();
+
+// mysqlConnection.query('select * from tbl_alumno',function(error,results,fields){
+//     if(error) throw error;
+//     console.log(results);
+// });
+
+// mysqlConnection.end();
+
+mysqlConnection.connect(function (err){
+    if(err){
+        console.error(err);
+        return;
+    }
+    else{
+        console.log('conectado a la base de datos')
+    }
+});
+module.exports = mysqlConnection;
+
